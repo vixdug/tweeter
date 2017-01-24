6c7c120fe964e306e545231d611b7bbe16f7e076
vixdug <victoria.duggan@shopify.com>
2017-01-24 07:55:44 +0000

    added JQuery to form

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+$(".new-tweet").on("keyup", "textarea", function() {
+var count = $(".new-tweet").find("textarea").val().length
+    var left = 140 - $(".new-tweet").find("textarea").val().length
+  var countdown= $(this).parent().find(".counter").text(left)
+    if (left < 0) {
+      countdown.css({"color":"red"})
+    }
+})
+});
