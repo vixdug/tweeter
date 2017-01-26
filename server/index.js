79ab6c20c816d761a8cc2194d2d3d3489af78f62
vixdug <victoria.duggan@shopify.com>
2017-01-26 23:20:45 +0000

    updated port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Basic express setup:
 
-const PORT          = 5000;
+const PORT          = process.env.PORT || 5000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
